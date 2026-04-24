d14cb323ce3de2cd88cc560498a5e481f0060f76
ryubyeongsun <133829802+ryubyeongsun@users.noreply.github.com>
2026-04-24 12:02:14 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 5.69 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] mats, String[][] park) {
+        int answer = -1;
+
+        for (int m = 0; m < mats.length; m++) {
+            int n = mats[m];
+
+            for (int i = 0; i < park.length; i++) {
+                for (int j = 0; j < park[0].length; j++) {
+
+                    if (park[i][j].equals("-1")) {
+
+                        if (i + n <= park.length && j + n <= park[0].length) {
+                            boolean chk = true;
+
+                            for (int z = i; z < i + n; z++) {
+                                for (int k = j; k < j + n; k++) {
+                                    if (!park[z][k].equals("-1")) {
+                                        chk = false;
+                                        break;
+                                    }
+                                }
+
+                                if (!chk) break;
+                            }
+
+                            if (chk) {
+                                answer = Math.max(answer, n);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
