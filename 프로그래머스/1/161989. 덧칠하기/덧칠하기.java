b58693e71d7b99b9ce42275e0dab66bd24002204
ryubyeongsun <133829802+ryubyeongsun@users.noreply.github.com>
2026-04-29 10:49:00 +0900

    [level 1] Title: 덧칠하기, Time: 3.95 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,12 +1,23 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        int paintedEnd = 0;
+        boolean[] chk = new boolean[n + 1];
 
-        for (int s : section) {
-            if (s > paintedEnd) {
+        for (int i = 0; i < section.length; i++) {
+            chk[section[i]] = true;
+        }
+
+        for (int i = 1; i <= n; i++) {
+
+            if (chk[i]) {
                 answer++;
-                paintedEnd = s + m - 1;
+
+                for (int j = i; j < i + m; j++) {
+                    if (j > n) break;
+                    chk[j] = false;
+                }
+
+                i = i + m - 1;
             }
         }
 
