e39591ebcaf2a1de3cbc41566ebfd2d2a2ff088c
ryubyeongsun <133829802+ryubyeongsun@users.noreply.github.com>
2026-04-29 10:24:21 +0900

    [level 1] Title: 덧칠하기, Time: 1.87 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int paintedEnd = 0;
+
+        for (int s : section) {
+            if (s > paintedEnd) {
+                answer++;
+                paintedEnd = s + m - 1;
+            }
+        }
+
+        return answer;
+    }
+}
