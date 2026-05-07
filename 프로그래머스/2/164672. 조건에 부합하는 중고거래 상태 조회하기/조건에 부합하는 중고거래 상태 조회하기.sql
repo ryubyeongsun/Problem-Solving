c29289be293cd8efea05e398839f18782b58112c
ryubyeongsun <133829802+ryubyeongsun@users.noreply.github.com>
2026-05-07 11:16:00 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,9 @@
+select BOARD_ID, WRITER_ID, TITLE, PRICE, 
+        CASE
+            WHEN STATUS = 'DONE' THEN "거래완료"
+            WHEN STATUS = "SALE" THEN "판매중"
+            WHEN STATUS = "RESERVED" THEN "예약중"
+        END AS STATUS
+from USED_GOODS_BOARD
+where CREATED_DATE = "2022-10-05"
+order by BOARD_ID DESC;
